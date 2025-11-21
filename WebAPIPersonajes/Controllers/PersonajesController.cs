bbd24eba51662a8343bd869cb93142ccf25a6853
ana <ana.lacima.ibercaja@gmail.com>
2025-11-21 13:54:37 +0100

    añadida vista index

---
+++ WebAPIPersonajes/Controllers/PersonajesController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPIPersonajes.Models;
+
+namespace WebAPIPersonajes.Controllers
+{
+    public class PersonajesController : Controller
+    {
+        // GET: PersonajesController
+        public ActionResult Index()
+        {
+            List<Personaje> listaPersonajes = new();
+            listaPersonajes.Add(new Personaje() { Id = 1, Name = "Superman", Force = 100, Hight = 190, canFly = true, BirthDate = new DateTime(1938, 6, 1) });
+            listaPersonajes.Add(new Personaje() { Id = 2, Name = "Batman", Force = 85, Hight = 185, canFly = false, BirthDate = new DateTime(1939, 5, 1) });
+            listaPersonajes.Add(new Personaje() { Id =3, Name = "Spiderman", Force = 90, Hight = 175, canFly = false, BirthDate = new DateTime(1962, 8, 1) });
+
+            return View(listaPersonajes);
+        }
+
+        // GET: PersonajesController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: PersonajesController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PersonajesController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PersonajesController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: PersonajesController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PersonajesController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: PersonajesController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ WebAPIPersonajes/Models/Personaje.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPIPersonajes.Models
+{
+    public class Personaje
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(100, ErrorMessage = "El nombre de la pizza debe tener entre 3 y 100 caracteres.", MinimumLength = 2)]
+        public string Name { get; set; }
+        [Range(0, 100, ErrorMessage = "La fuerza debe estar entre 0 y 100.")]
+        public int Force { get; set; }
+        [Range(0, 200, ErrorMessage = "La altura debe estar entre 0 y 200.")]
+        public int Hight { get; set; }
+        public bool canFly { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
