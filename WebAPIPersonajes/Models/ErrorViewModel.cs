992cc788bffbdcc6eea68c618c8566cf2b4e721e
ana <ana.lacima.ibercaja@gmail.com>
2025-11-21 13:23:16 +0100

    Agregar archivos de proyecto.

---
+++ WebAPIPersonajes/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebAPIPersonajes.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
